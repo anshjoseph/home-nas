99a7f70c96e01afa913095560f20c3034cdef370
R ansh joseph <ranshjosrph@gmail.com>
2021-10-14 10:05:13 +0530

    solving bug (now scripts are updating)

--- home-nas/HOME_NAS.py
+++ home-nas/HOME_NAS.py
@@ -4,7 +4,7 @@ import sys
 BASE_DIR = str(Path(__file__).resolve().parent.parent)
 command = sys.argv
 maincom = command[1]
-
+ 
 if maincom.lower() == "-run" or maincom.lower() == "-r":
     os.system(f"{BASE_DIR}\\RUN.bat")
 elif maincom.lower() == "-stop" or maincom.lower() == "-stop":
@@ -12,9 +12,14 @@ elif maincom.lower() == "-stop" or maincom.lower() == "-stop":
 elif  maincom.lower() == "-install" or maincom.lower() == "-i":
     os.system(f"setx PATH \"{Path(__file__).resolve()};%PATH%\"")
     
+    
+
     # run install
     com1 = f"cd .. && {BASE_DIR}\\install.exe"
     os.system(com1)
+
+    #getting script ready
+    os.system(f"cd .. && cd application && RUN_SCRIPT.exe")
 
     #get checking output of install
     with open(f"{BASE_DIR}//BASE_DIR.txt",'r') as paths:
